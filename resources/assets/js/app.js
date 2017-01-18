8f62ec438c56a76a234e22e06440b159a4ac9327
Daniel <danielg@healthpro.com>
2017-01-18 14:39:07 +0200

    change order

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,12 @@
+window._ = require('lodash');
 
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+window.$ = window.jQuery = require('jquery');
 /**
  * First we will load all of this project's JavaScript dependencies which
  * include Vue and Vue Resource. This gives a great starting point for
